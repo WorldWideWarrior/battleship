437164ef99d4e177d656524dd987cda67511a6ce
SchalterDev <schalter.dev@gmail.com>
2017-12-19 20:03:02 +0100

    Connect to server, send and receive name
    when state "setup" is send show prompt to input name
    name is send to server and updated for opponent

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,13 @@
 import io from 'socket.io-client';
 
+let clientId;
+let socket;
+
+let actualState;
+let ownName;
+let opponentName;
+let ships;
+
 function generateTable(table, rows, columns) {
     for (let row = 0; row < rows; row++) {
         const rowElement = $('<tr></tr>');
@@ -18,30 +26,70 @@ function generateTable(table, rows, columns) {
     }
 }
 
-$(document).ready(() => {
+function showPlayerInput() {
     // show modal on startup
     $('#player-modal').modal({
         backdrop: 'static',
         keyboard: false,
     });
+}
 
+function sendPlayerName() {
+    socket.emit('set-name', ownName);
+}
+
+function onGameState(snapshot) {
+    actualState = snapshot.state;
+    if (snapshot.state === 'waitingForSecondPlayer') {
+
+    } else if (snapshot.state === 'setup') {
+        showPlayerInput();
+    } else if (snapshot.state === 'attack') {
+
+    } else if (snapshot.state === 'defence') {
+
+    } else if (snapshot.state === 'otherPlayerDisconnect') {
+
+    } else if (snapshot.state === 'gameOver') {
+
+    }
+}
+
+$(document).ready(() => {
     // create tables
     const fieldOwn = $('#field-own');
     generateTable(fieldOwn, 10, 10);
     const fieldOpponent = $('#field-opponent');
     generateTable(fieldOpponent, 10, 10);
 
-    const socket = io('localhost:3000');
+    socket = io('localhost:3000');
+
+    socket.on('connect', () => {
+        socket.emit('client-id', clientId);
+    });
+
+    socket.on('client-id', (id) => {
+        clientId = id;
+    });
+
+    socket.on('set-name', (name) => {
+        opponentName = name;
+        $('#player2Name').text(`Opponent: ${name}`);
+    });
+
+    socket.on('game-state', (snapshot) => {
+        onGameState(snapshot);
+    });
 });
 
-// validate player names
+// validate player name
 $('#buttonReadyPlayerModal').click(() => {
-    const player1 = $('#player1Input').val();
-    const player2 = $('#player2Input').val();
+    const playerName = $('#playerNameInput').val();
 
-    if (player1 !== '' && player2 !== '' && player1 !== player2) {
-        $('#player1Name').text(`Player 1: ${player1}`);
-        $('#player2Name').text(`Player 2: ${player2}`);
+    if (playerName) {
+        $('#player1Name').text(`You: ${playerName}`);
         $('#player-modal').modal('hide');
+        ownName = playerName;
+        sendPlayerName();
     }
 });
