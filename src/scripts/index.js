0f10f87fbdab78841f6025f1425d1c78a90b94e7
SchalterDev <schalter.dev@gmail.com>
2017-12-19 14:15:55 +0100

    Added semicolons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,28 +6,28 @@ $(document).ready(function () {
             backdrop: 'static',
             keyboard: false
         }
-    )
+    );
 
     //create tables
     var fieldOwn = $('#field-own');
-    generateTable(fieldOwn, 10,10)
+    generateTable(fieldOwn, 10,10);
     var fieldOpponent = $('#field-opponent');
-    generateTable(fieldOpponent, 10,10)
+    generateTable(fieldOpponent, 10,10);
 
     var socket = io("localhost:3000")
 });
 
 function generateTable(table, rows, columns) {
     for(var row = 0; row < rows; row++) {
-        var rowElement = $('<tr></tr>')
+        var rowElement = $('<tr></tr>');
         for(var column = 0; column < columns; column++) {
-            var columnElement = $('<td/>')
+            var columnElement = $('<td/>');
 
             (function(row, column){
                 columnElement.on('click', function () {
                     console.log("Click: " + row + ", " + column);
                 })
-            })(row, column)
+            })(row, column);
 
             rowElement.append(columnElement);
         }
@@ -37,12 +37,12 @@ function generateTable(table, rows, columns) {
 
 //validate player names
 $("#buttonReadyPlayerModal").click( () => {
-    var player1 = $('#player1Input').val()
-    var player2 = $('#player2Input').val()
+    var player1 = $('#player1Input').val();
+    var player2 = $('#player2Input').val();
 
     if(player1 !== "" && player2 !== "" && player1 !== player2) {
-        $('#player1Name').text("Player 1: " + player1)
-        $('#player2Name').text("Player 2: " + player2)
+        $('#player1Name').text("Player 1: " + player1);
+        $('#player2Name').text("Player 2: " + player2);
         $('#player-modal').modal('hide')
     }
 
