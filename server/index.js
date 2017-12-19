2984d6a0abff0a1236307d859e5fb1bf629d03ae
SchalterDev <schalter.dev@gmail.com>
2017-12-19 17:31:02 +0100

    Eslint: 4 spaces instead of 2

--- server/index.js
+++ server/index.js
@@ -3,16 +3,15 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const serveStatic = require('serve-static');
 
+require('./lobby');
+
 app.use(serveStatic('../static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-io.on('connection', (socket) => {
-  console.log(`a user connected ${socket}`);
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
+    new Lobby(io);
 });
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,47 +1,47 @@
 import io from 'socket.io-client';
 
 function generateTable(table, rows, columns) {
-  for (let row = 0; row < rows; row++) {
-    const rowElement = $('<tr></tr>');
-    for (let column = 0; column < columns; column++) {
-      const columnElement = $('<td/>');
+    for (let row = 0; row < rows; row++) {
+        const rowElement = $('<tr></tr>');
+        for (let column = 0; column < columns; column++) {
+            const columnElement = $('<td/>');
 
-      (function (clickRow, clickColumn) {
-        columnElement.on('click', () => {
-          console.log(`Click: ${clickRow}, ${clickColumn}`);
-        });
-      }(row, column));
+            (function (clickRow, clickColumn) {
+                columnElement.on('click', () => {
+                    console.log(`Click: ${clickRow}, ${clickColumn}`);
+                });
+            }(row, column));
 
-      rowElement.append(columnElement);
+            rowElement.append(columnElement);
+        }
+        table.append(rowElement);
     }
-    table.append(rowElement);
-  }
 }
 
 $(document).ready(() => {
-  // show modal on startup
-  $('#player-modal').modal({
-    backdrop: 'static',
-    keyboard: false,
-  });
+    // show modal on startup
+    $('#player-modal').modal({
+        backdrop: 'static',
+        keyboard: false,
+    });
 
-  // create tables
-  const fieldOwn = $('#field-own');
-  generateTable(fieldOwn, 10, 10);
-  const fieldOpponent = $('#field-opponent');
-  generateTable(fieldOpponent, 10, 10);
+    // create tables
+    const fieldOwn = $('#field-own');
+    generateTable(fieldOwn, 10, 10);
+    const fieldOpponent = $('#field-opponent');
+    generateTable(fieldOpponent, 10, 10);
 
-  const socket = io('localhost:3000');
+    const socket = io('localhost:3000');
 });
 
 // validate player names
 $('#buttonReadyPlayerModal').click(() => {
-  const player1 = $('#player1Input').val();
-  const player2 = $('#player2Input').val();
+    const player1 = $('#player1Input').val();
+    const player2 = $('#player2Input').val();
 
-  if (player1 !== '' && player2 !== '' && player1 !== player2) {
-    $('#player1Name').text(`Player 1: ${player1}`);
-    $('#player2Name').text(`Player 2: ${player2}`);
-    $('#player-modal').modal('hide');
-  }
+    if (player1 !== '' && player2 !== '' && player1 !== player2) {
+        $('#player1Name').text(`Player 1: ${player1}`);
+        $('#player2Name').text(`Player 2: ${player2}`);
+        $('#player-modal').modal('hide');
+    }
 });
